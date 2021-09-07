8898a499f7850f63f85a2d2a3728fd44fc3a4520
kuherrn <200479344@student.georgianc.on.ca>
2021-09-07 13:43:54 -0400

    Added color to title

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
   </head>
 
   <body>
-    <h1><?= $pgData->getTitle() ?></h1>
+    <h1 style="color: red"><?= $pgData->getTitle() ?></h1>
     <p><?= $pgData->getContent() ?></p>
   </body>
 </html>
