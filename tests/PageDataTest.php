5426f1981823bec4ca47c98f1df1f331dd228b38
kuherrn <200479344@student.georgianc.on.ca>
2021-09-07 17:23:22 -0400

    Added comments and questions

--- tests/PageDataTest.php
+++ tests/PageDataTest.php
@@ -3,22 +3,25 @@
     use PHPUnit\Framework\TestCase;
     require_once getcwd() . "/src/PageData.php";
 
+    // Extends allows 'PageDataTest' to inherit the methods from 'TestCase'
     class PageDataTest extends TestCase {
         
         public function testCorrectArgumentsResultsInSuccessfulInstantiation() {
+            /* PageData::class will return the fully qualified name of "PageData" class including the namespace
+            https://stackoverflow.com/questions/30770148/what-is-class-in-php */
             $this->assertInstanceOf(PageData::class, new PageData("title", "content"));
         }
 
+        /* What does 'exceptException' do?
+        https://phpunit.readthedocs.io/en/latest/writing-tests-for-phpunit.html#testing-exceptions */
         public function testMissingArgumentsCausesFailedInstantiation () {
             $this->expectException(ArgumentCountError::class);
-
             new PageData();
         }
 
         public function testIncorrectArgumentCausesFailedInstantiation() {
             $this->expectException(TypeError::class);
-
-            new PageData([1,2,3], [1,2,3]);
+            new PageData();
         }
 
         public function testGetTitleSuccessfullyReturnsTitle () {
